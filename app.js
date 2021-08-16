117173383bda47a576f6bac1894e8ca4342b01d6
Petros <skouna3@gmail.com>
2021-08-16 15:58:24 +0300

    reset button final

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ const maxPlayers = 6;
 let activePlayers = 2;
 let resetGameButton = document.querySelector(".reset-game")
 let playerDeleteButton = document.querySelector(".player-delete-button")
+const list = document.querySelector("#list")
 
-const playerDelete = (ev) => {
+
+playerDeleteButton.addEventListener('click', (ev) => {
     ev.target.closest(".col-player").remove()
-}
-
-resetGameButton.addEventListener('click', playerDelete)
+})
 
 
+resetGameButton.addEventListener('click', () => {
+    let childrens = document.querySelectorAll('#list .col-player')
+    for (let i = 0; i < childrens.length; i++) {
+        childrens[i].remove()
+    }
+})
 
-playerDeleteButton.addEventListener('click', playerDelete)
+
+
